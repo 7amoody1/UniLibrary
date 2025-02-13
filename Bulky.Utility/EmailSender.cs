e4bb9bd65310b867006ae91bd79d1c3de0b37c4b
mohammed <7amoody1alfreh@gmail.com>
2025-02-13 22:59:25 +0300

    add-appsettings

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -9,25 +9,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BulkyBook.Utility {
-    public class EmailSender : IEmailSender {
-        public string SendGridSecret { get; set; }
-
-        public EmailSender(IConfiguration _config) {
-            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
-        }
-
-        public Task SendEmailAsync(string email, string subject, string htmlMessage) {
-            //logic to send email
-
-            var client = new SendGridClient(SendGridSecret);
-
-            var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
-            var to = new EmailAddress(email);
-            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
-
-            return client.SendEmailAsync(message);
-
-
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            await Task.CompletedTask;
         }
     }
 }
